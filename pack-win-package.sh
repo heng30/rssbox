cd0ea602a2e9b95f6828511bb86dd13176f2ed26
blueJpg <2238288979@qq.com>
2023-09-29 17:44:46 +0800

    [*] modify files

--- pack-win-package.sh
+++ pack-win-package.sh
@@ -2,9 +2,11 @@
 
 version=`git describe --tags --abbrev=0`
 machine=`uname -m`
-output_dir=rssbox-win-$machine
-target_name=rssbox-$version.exe
-target_path=./target/release/rssbox.exe
+
+target=rssbox
+output_dir=$target-win-$machine
+target_name=$target-$version.exe
+target_path=./target/release/$target.exe
 dll_dir=./win/dll
 
 if [ -f $target_path ]; then
